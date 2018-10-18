a3050a95eb39d01bcbdc87f019ca5f939e16c79d
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 10:05:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object)
-  newObject[key] = `${value}`
-  return newObject
+  var newObject = Object.assign({}, object);
+  newObject[key] = `${value}`;
+  return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = `${value}`
-  return object
+  object[key] = `${value}`;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
